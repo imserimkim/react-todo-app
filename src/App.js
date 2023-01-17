25b74eacdcf0c8c07a86cbecb2bee4cd8e159625
cj <tpfla22@naver.com>
2023-01-17 21:30:59 +0900

    01/17-2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
