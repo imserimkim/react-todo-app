fcbd313fe13efaba2461d7d81c1a2e8319483fe8
cj <tpfla22@naver.com>
2023-01-18 21:27:08 +0900

    01/18 todo-list UI 작성

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import logo from './logo.svg';
+
+// 클래스 컴포넌트
+import React, {Component} from "react";  //리액트 라이브러리에서 가져 옴
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
+export default class App extends Component { //컴포넌트를 사용할 수 있게 확장
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-    </div>
-  );
+  state = {
+    todoData : [
+      {
+        id: "1", 
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: '2',
+        title: '청소하기',
+        completed: false 
+      }
+    ]
+  }
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px", 
+    borderRadius: "50%",    // css -> jsx : ( border-radious -> borderRadius )
+    cursor:"pointer",
+    float:"right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px dotted #ccc",
+      textDecoration: 'none'
+    }
+  }
+
+
+  handleClick = ( id ) =>{
+    /* 할 일 : 클릭한 목록 지우기 */
+    let newTodoData = this.todoData.filter(data => data.id !== id)  // data.id == id 같은 것은 지우고 다른것만 가져오기
+    console.log('newTodoData', newTodoData)
+  }
+  render() {                                 // 클래스 컴포넌트에서는 render 필요
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+    
+          {this.todoData.map((data) => (
+              <div style={this.getStyle()} key={data.id}>
+                  <input type="checkbox" defaultChecked={false} /> {/* defaultChecked: 체크 안됨이 기본*/}
+                  {data.title}
+                  <button style={this.btnStyle} onClick={()=> this.handleClick(data.id)}>x</button>
+              </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
